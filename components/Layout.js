83e9a8023c3b09564d1b89584ac4c87ee2cf552c
Johan Burgos <jsburgos@uninorte.edu.co>
2021-06-21 10:47:07 -0500

    87-16

--- components/Layout.js
+++ components/Layout.js
@@ -1,33 +1,37 @@
 import React from 'react'
 import Head from 'next/head'
-import Sidebar from './Sidebar'
-import { useRouter } from 'next/router'
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+import { useRouter } from 'next/router';
 
 const Layout = ({children}) => {
 
-    // routing de next
-
+    // Hook de routing
     const router = useRouter();
 
-    return(
+    return ( 
         <>
             <Head>
-                <title>CRM -Administracion de Clientes</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-                <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
+                <title>CRM - Administración de Clientes</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossOrigin="anonymous" />
+                <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
             </Head>
 
-            {router.pathname === '/login' || '/nuevacuenta' ? (
+            {router.pathname === '/login' || router.pathname === '/nuevacuenta' ? (
                 <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
-                    {children}
+                    <div>
+                        {children}
+                    </div>
+                    
                 </div>
             ) : (
                 <div className="bg-gray-200 min-h-screen">
                     <div className="sm:flex min-h-screen">
                         <Sidebar />
                         
-                        
+                       
                         <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+                            <Header />
                             {children}
                         </main>
                         
@@ -35,7 +39,7 @@ const Layout = ({children}) => {
                 </div>
             )}
         </>
-    )
+     );
 }
-
+ 
 export default Layout;
