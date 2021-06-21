0a827692c8b96cf4ef762148b52a35dad66a6478
Johan Burgos <jsburgos@uninorte.edu.co>
2021-06-21 01:37:09 -0500

    Comenzando cliente

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const NuevaCuenta = () => {
+    return(
+        <>
+            <Layout>
+                <h1 className="text-center">Nueva Cuenta</h1>
+            </Layout>
+        </>
+    )
+}
+
+export default NuevaCuenta
